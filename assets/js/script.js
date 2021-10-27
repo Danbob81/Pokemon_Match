8f1c13bf5da4192186a78c25b73f1413dec84660
Daniel Higginson <82511196+Danbob81@users.noreply.github.com>
2021-10-27 17:59:45 +0100

    remove function playGame2 for cleaner functionality across devices. Set createBoard as first function to run when page is loaded

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,313 +1,162 @@
-function playGame() {
+// function playGame() {
 
-    // card options
-    const cardArray = [{
-            name: 'bulbasaur',
-            img: 'assets/images/bulbasaur.png'
-        },
-        {
-            name: 'bulbasaur',
-            img: 'assets/images/bulbasaur.png'
-        },
-        {
-            name: 'charmander',
-            img: 'assets/images/charmander.png'
-        },
-        {
-            name: 'charmander',
-            img: 'assets/images/charmander.png'
-        },
-        {
-            name: 'eevee',
-            img: 'assets/images/eevee.png'
-        },
-        {
-            name: 'eevee',
-            img: 'assets/images/eevee.png'
-        },
-        {
-            name: 'jigglypuff',
-            img: 'assets/images/jigglypuff.png'
-        },
-        {
-            name: 'jigglypuff',
-            img: 'assets/images/jigglypuff.png'
-        },
-        {
-            name: 'pikachu',
-            img: 'assets/images/pikachu.png'
-        },
-        {
-            name: 'pikachu',
-            img: 'assets/images/pikachu.png'
-        },
-        // {
-        //     name: 'psyduck',
-        //     img: 'assets/images/psyduck.png'
-        // },
-        // {
-        //     name: 'psyduck',
-        //     img: 'assets/images/psyduck.png'
-        // },
-        // {
-        //     name: 'snorlax',
-        //     img: 'assets/images/snorlax.png'
-        // },
-        // {
-        //     name: 'snorlax',
-        //     img: 'assets/images/snorlax.png'
-        // },
-        {
-            name: 'squirtle',
-            img: 'assets/images/squirtle.png'
-        },
-        {
-            name: 'squirtle',
-            img: 'assets/images/squirtle.png'
-        },
-    ]
+// card options
+const cardArray = [{
+        name: 'bulbasaur',
+        img: 'assets/images/bulbasaur.png'
+    },
+    {
+        name: 'bulbasaur',
+        img: 'assets/images/bulbasaur.png'
+    },
+    {
+        name: 'charmander',
+        img: 'assets/images/charmander.png'
+    },
+    {
+        name: 'charmander',
+        img: 'assets/images/charmander.png'
+    },
+    {
+        name: 'eevee',
+        img: 'assets/images/eevee.png'
+    },
+    {
+        name: 'eevee',
+        img: 'assets/images/eevee.png'
+    },
+    {
+        name: 'jigglypuff',
+        img: 'assets/images/jigglypuff.png'
+    },
+    {
+        name: 'jigglypuff',
+        img: 'assets/images/jigglypuff.png'
+    },
+    {
+        name: 'pikachu',
+        img: 'assets/images/pikachu.png'
+    },
+    {
+        name: 'pikachu',
+        img: 'assets/images/pikachu.png'
+    },
+    // {
+    //     name: 'psyduck',
+    //     img: 'assets/images/psyduck.png'
+    // },
+    // {
+    //     name: 'psyduck',
+    //     img: 'assets/images/psyduck.png'
+    // },
+    // {
+    //     name: 'snorlax',
+    //     img: 'assets/images/snorlax.png'
+    // },
+    // {
+    //     name: 'snorlax',
+    //     img: 'assets/images/snorlax.png'
+    // },
+    {
+        name: 'squirtle',
+        img: 'assets/images/squirtle.png'
+    },
+    {
+        name: 'squirtle',
+        img: 'assets/images/squirtle.png'
+    },
+]
 
-    cardArray.sort(() => 0.5 - Math.random())
+cardArray.sort(() => 0.5 - Math.random())
 
-
-
-    const grid = document.querySelector('.grid')
-    const resultDisplay = document.querySelector('#result')
-    let cardsChosen = []
-    let cardsChosenId = []
-    let cardsWon = []
-
-    // create board
-    function createBoard() {
-        for (let i = 0; i < cardArray.length; i++) {
-            let card = document.createElement('img')
-            card.setAttribute('src', 'assets/images/pokeball.png')
-            card.setAttribute('data-id', i)
-            card.addEventListener('click', flipCard)
-            grid.appendChild(card)
-        }
+// create board
+function createBoard() {
+    let grid = document.querySelector('.grid');
+    for (let i = 0; i < cardArray.length; i++) {
+        let card = document.createElement('img')
+        card.setAttribute('src', 'assets/images/pokeball.png')
+        card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
+        grid.appendChild(card)
     }
-
-    // check for matches
-    function checkForMatch() {
-        let cards = document.querySelectorAll('img')
-        const optionOneId = cardsChosenId[0]
-        const optionTwoId = cardsChosenId[1]
-
-        if (optionOneId === optionTwoId) {
-            cards[optionOneId].setAttribute('src', 'assets/images/pokeball.png')
-            cards[optionTwoId].setAttribute('src', 'assets/images/pokeball.png')
-                // alert('You have clicked the same image!')
-        } else if (cardsChosen[0] === cardsChosen[1]) {
-            // alert('You found a match!')
-            cards[optionOneId].removeEventListener('click', flipCard, )
-            cards[optionTwoId].removeEventListener('click', flipCard, )
-            cardsWon.push(cardsChosen)
-            playAudio1()
-        } else {
-            cards[optionOneId].setAttribute('src', 'assets/images/pokeball.png')
-            cards[optionTwoId].setAttribute('src', 'assets/images/pokeball.png')
-            playAudio3()
-                // alert('Sorry, try again!')
-        }
-        cardsChosen = []
-        cardsChosenId = []
-        resultDisplay.textContent = cardsWon.length
-        if (cardsWon.length === cardArray.length / 2) {
-            resultDisplay.textContent = ' Congratulaions! You found them all!'
-        }
-    }
-
-    // flip your card
-    function flipCard() {
-        let cardId = this.getAttribute('data-id')
-        cardsChosen.push(cardArray[cardId].name)
-        cardsChosenId.push(cardId)
-        playAudio2()
-        this.setAttribute('src', cardArray[cardId].img)
-        if (cardsChosen.length === 2) {
-            setTimeout(checkForMatch, 500)
-        }
-    }
-    // card sounds
-
-    function playAudio1() {
-        let sound1 = document.getElementById('sound1')
-        sound1.play();
-    }
-
-    function playAudio2() {
-        let sound2 = document.getElementById('sound2')
-        sound2.play();
-    }
-
-    function playAudio3() {
-        let sound3 = document.getElementById('sound3')
-        sound3.play();
-    }
-
-    createBoard()
 }
 
-function playGame2() {
 
-    // card options
-    const cardArray = [{
-            name: 'bulbasaur',
-            img: 'assets/images/bulbasaur.png'
-        },
-        {
-            name: 'bulbasaur',
-            img: 'assets/images/bulbasaur.png'
-        },
-        {
-            name: 'charmander',
-            img: 'assets/images/charmander.png'
-        },
-        {
-            name: 'charmander',
-            img: 'assets/images/charmander.png'
-        },
-        {
-            name: 'eevee',
-            img: 'assets/images/eevee.png'
-        },
-        {
-            name: 'eevee',
-            img: 'assets/images/eevee.png'
-        },
-        {
-            name: 'jigglypuff',
-            img: 'assets/images/jigglypuff.png'
-        },
-        {
-            name: 'jigglypuff',
-            img: 'assets/images/jigglypuff.png'
-        },
-        {
-            name: 'pikachu',
-            img: 'assets/images/pikachu.png'
-        },
-        {
-            name: 'pikachu',
-            img: 'assets/images/pikachu.png'
-        },
-        // {
-        //     name: 'psyduck',
-        //     img: 'assets/images/psyduck.png'
-        // },
-        // {
-        //     name: 'psyduck',
-        //     img: 'assets/images/psyduck.png'
-        // },
-        // {
-        //     name: 'snorlax',
-        //     img: 'assets/images/snorlax.png'
-        // },
-        // {
-        //     name: 'snorlax',
-        //     img: 'assets/images/snorlax.png'
-        // },
-        {
-            name: 'squirtle',
-            img: 'assets/images/squirtle.png'
-        },
-        {
-            name: 'squirtle',
-            img: 'assets/images/squirtle.png'
-        },
-    ]
-
-    cardArray.sort(() => 0.5 - Math.random())
+const resultDisplay = document.querySelector('#result')
+let cardsChosen = []
+let cardsChosenId = []
+let cardsWon = []
 
 
 
-    const grid = document.querySelector('.modal-body .grid')
-    const resultDisplay = document.querySelector('#result')
-    let cardsChosen = []
-    let cardsChosenId = []
-    let cardsWon = []
+// check for matches
+function checkForMatch() {
+    let cards = document.querySelectorAll('img')
+    const optionOneId = cardsChosenId[0]
+    const optionTwoId = cardsChosenId[1]
 
-    // create board
-    function createBoard() {
-        for (let i = 0; i < cardArray.length; i++) {
-            let card = document.createElement('img')
-            card.setAttribute('src', 'assets/images/pokeball.png')
-            card.setAttribute('data-id', i)
-            card.addEventListener('click', flipCard)
-            grid.appendChild(card)
-        }
-    }
-
-    // check for matches
-    function checkForMatch() {
-        let cards = document.querySelectorAll('img')
-        const optionOneId = cardsChosenId[0]
-        const optionTwoId = cardsChosenId[1]
-
-        if (optionOneId === optionTwoId) {
-            cards[optionOneId].setAttribute('src', 'assets/images/pokeball.png')
-            cards[optionTwoId].setAttribute('src', 'assets/images/pokeball.png')
-                // alert('You have clicked the same image!')
-        } else if (cardsChosen[0] === cardsChosen[1]) {
-            // alert('You found a match!')
-            cards[optionOneId].removeEventListener('click', flipCard, )
-            cards[optionTwoId].removeEventListener('click', flipCard, )
-            cardsWon.push(cardsChosen)
-            playAudio1()
-        } else {
-            cards[optionOneId].setAttribute('src', 'assets/images/pokeball.png')
-            cards[optionTwoId].setAttribute('src', 'assets/images/pokeball.png')
-            playAudio3()
-                // alert('Sorry, try again!')
-        }
-        cardsChosen = []
-        cardsChosenId = []
-        resultDisplay.textContent = cardsWon.length
-        if (cardsWon.length === cardArray.length / 2) {
-            resultDisplay.textContent = ' Congratulaions! You found them all!'
-        }
-    }
-
-    // flip your card
-    function flipCard() {
-        let cardId = this.getAttribute('data-id')
-        cardsChosen.push(cardArray[cardId].name)
-        cardsChosenId.push(cardId)
-        playAudio2()
-        this.setAttribute('src', cardArray[cardId].img)
-        if (cardsChosen.length === 2) {
-            setTimeout(checkForMatch, 500)
-        }
-    }
-    // card sounds
-
-    function playAudio1() {
-        let sound1 = document.getElementById('sound1')
-        sound1.play();
-    }
-
-    function playAudio2() {
-        let sound2 = document.getElementById('sound2')
-        sound2.play();
-    }
-
-    function playAudio3() {
-        let sound3 = document.getElementById('sound3')
-        sound3.play();
-    }
-
-    createBoard()
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    if (window.matchMedia('(min-width: 576px)').matches) {
-        playGame()
+    if (optionOneId === optionTwoId) {
+        cards[optionOneId].setAttribute('src', 'assets/images/pokeball.png')
+        cards[optionTwoId].setAttribute('src', 'assets/images/pokeball.png')
+    } else if (cardsChosen[0] === cardsChosen[1]) {
+        cards[optionOneId].removeEventListener('click', flipCard, )
+        cards[optionTwoId].removeEventListener('click', flipCard, )
+        cardsWon.push(cardsChosen)
+        playAudio1()
     } else {
-        playGame2()
+        cards[optionOneId].setAttribute('src', 'assets/images/pokeball.png')
+        cards[optionTwoId].setAttribute('src', 'assets/images/pokeball.png')
+        playAudio3()
     }
-})
+    cardsChosen = []
+    cardsChosenId = []
+    resultDisplay.textContent = cardsWon.length
+    if (cardsWon.length === cardArray.length / 2) {
+        resultDisplay.textContent = ' Congratulaions! You found them all!'
+    }
+}
 
-window.addEventListener('resize', function() {
+// flip your card
+function flipCard() {
+    let cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenId.push(cardId)
+    playAudio2()
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length === 2) {
+        setTimeout(checkForMatch, 500)
+    }
+}
+// card sounds
 
-})
+function playAudio1() {
+    let sound1 = document.getElementById('sound1')
+    sound1.play();
+}
+
+function playAudio2() {
+    let sound2 = document.getElementById('sound2')
+    sound2.play();
+}
+
+function playAudio3() {
+    let sound3 = document.getElementById('sound3')
+    sound3.play();
+}
+
+document.addEventListener('DOMContentLoaded', createBoard);
+
+// createBoard()
+// }
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     if (window.matchMedia('(min-width: 576px)').matches) {
+//         playGame()
+//     } else {
+//         playGame2()
+//     }
+// })
+
+// window.addEventListener('resize', function() {
+
+// })
